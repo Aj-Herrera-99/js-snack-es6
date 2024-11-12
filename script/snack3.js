58509fd8c637db3a3c0f528e4a9d40a7893c3334
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-12 15:41:28 +0100

    snack3

--- script/snack3.js
+++ script/snack3.js
@@ -7,16 +7,56 @@ che hanno la posizione compresa tra i due numeri.
  */
 // DOM elements selection
 const output3 = document.getElementById("snack3");
+const arr = [];
+const len = 10;
+const bound1 = 3;
+const bound2 = 8;
+for (let i = 0; i < len; i++) {
+    arr.push(getRndInteger(1, 99));
+}
+const nuovoArr = insertInRange(arr, bound1, bound2);
+output3.innerHTML = "Array:<br>";
+for (const el of arr) {
+    output3.innerHTML += el + " ";
+}
+
+if (nuovoArr.length) {
+    output3.innerHTML += `<br>Nuovo array in range ${bound1}-${bound2}<br>`;
+    for (const el of nuovoArr) {
+        output3.innerHTML += el + " ";
+    }
+} else {
+    output3.innerHTML += "<br>Nuovo array vuoto";
+}
+console.log(arr);
+console.log(nuovoArr);
+
 
 //! functions
 /**
- * 
- * @param {*} arr 
- * @param {*} n1 
- * @param {*} n2 
- * @returns 
+ *
+ * @param {number[]} arr
+ * @param {number} n1
+ * @param {number} n2
+ * @returns
  */
-function insertInRange(arr, n1, n2){
+function insertInRange(arr, n1, n2) {
+    if (n1 >= arr.length || n2 >= arr.length || n1 < 0 || n2 < 0) {
+        return [];
+    }
+    let left, right;
+    if (n1 < n2) {
+        left = n1;
+        right = n2;
+    } else if (n1 > n2) {
+        left = n2;
+        right = n1;
+    } else {
+        return [];
+    }
     const newArr = [];
-    return newArr
+    for (let i = left; i <= right; i++) {
+        newArr.push(arr[i]);
+    }
+    return newArr;
 }
