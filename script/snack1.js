41fb08a08aba6b95605cc14653a8b7bd86f6a908
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-12 14:46:33 +0100

    stampo a schermo e in pagina la bici con peso minore: trovo e assegno ad una variabile indexLeggera l'indice della bici con peso minore ciclando in un for, in tal modo tramite indice accedo direttamente all'oggetto dandomi la possibilità di usare i suoi metodi evitando di usare variabili esterne che referenziano alla bici con peso minore con impossibilità di usare i metodi di quest'ultimo

--- script/snack1.js
+++ script/snack1.js
@@ -5,12 +5,14 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare a schermo la bici con peso minore.
  */
 
+// DOM elements selection
+const output = document.getElementById("snack1");
 // Dichiarazione oggetti bici
 const bici1 = {
     nome: "bici1",
     peso: 12,
     descrizione: function () {
-        console.log("nome: " + this.nome + " peso: " + this.peso);
+        return "nome: " + this.nome + " peso: " + this.peso
     },
 };
 
@@ -18,7 +20,7 @@ const bici2 = {
     nome: "bici2",
     peso: 15,
     descrizione: function () {
-        console.log("nome: " + this.nome + " peso: " + this.peso);
+        return "nome: " + this.nome + " peso: " + this.peso
     },
 };
 
@@ -26,7 +28,7 @@ const bici3 = {
     nome: "bici3",
     peso: 18,
     descrizione: function () {
-        console.log("nome: " + this.nome + " peso: " + this.peso);
+        return "nome: " + this.nome + " peso: " + this.peso
     },
 };
 
@@ -34,7 +36,7 @@ const bici4 = {
     nome: "bici4",
     peso: 22,
     descrizione: function () {
-        console.log("nome: " + this.nome + " peso: " + this.peso);
+        return "nome: " + this.nome + " peso: " + this.peso
     },
 };
 
@@ -42,10 +44,20 @@ const bici5 = {
     nome: "bici5",
     peso: 10,
     descrizione: function () {
-        console.log("nome: " + this.nome + " peso: " + this.peso);
+        return "nome: " + this.nome + " peso: " + this.peso
     },
 };
 
-const listaBici = [bici1, bici2, bici3, bici4, bici5];
-console.log(listaBici);
+// inserimento oggetti bici in un array
+const arrayDiBici = [bici1, bici2, bici3, bici4, bici5];
+// ricerca indice della bici con peso minore
+let indexLeggera = 0;
+for(let i = 1; i<arrayDiBici.length; i++){
+    if(arrayDiBici[i].peso < arrayDiBici[indexLeggera].peso){
+        indexLeggera = i;
+    }
+}
+
+console.log(arrayDiBici[indexLeggera].descrizione());
+output.append(arrayDiBici[indexLeggera].descrizione());
 
